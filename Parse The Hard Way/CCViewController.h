b2291cfb81c76ec0135637227f62a8fa0b158323
Eliot Arntz <arntzel@gmail.com>
2013-12-01 15:54:33 +0100

    Initial Commit

---
+++ Parse The Hard Way/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Parse The Hard Way
+//
+//  Created by Eliot Arntz on 12/1/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Parse The Hard WayTests/Parse_The_Hard_WayTests.m
@@ -0,0 +1,34 @@
+//
+//  Parse_The_Hard_WayTests.m
+//  Parse The Hard WayTests
+//
+//  Created by Eliot Arntz on 12/1/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Parse_The_Hard_WayTests : XCTestCase
+
+@end
+
+@implementation Parse_The_Hard_WayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
